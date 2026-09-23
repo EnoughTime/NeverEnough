1edbfcdd16200a5eff7b2096ea849c0cf47bffc5
EnoughTime <gabrielvu210@gmail.com>
2026-09-23 07:26:41 -0600

    Update Variables and conditionals.py

--- Variables and conditionals.py
+++ Variables and conditionals.py
@@ -1,6 +1,6 @@
 # In Python, variables store a value; conditionals decide whether that value is correct or not. This is called Boolean logic, and it is handled by True or False.
 
-Example:
+# Example:
 v = 1
 n = 2
 if n == 2:
@@ -11,3 +11,15 @@ if n == 2:
 # if: if the condition is True, execute the lines inside the if statement.
 # elif: If the first 'if' condition is not True, evaluate this other option.
 # else: If the previous `if` (and other `elif`s) evaluates to false, execute this.
+
+# Example:
+v = 1
+n = 2
+if n == 4:
+    print("n is 4")
+elif n == 1:
+    print("n is 1")
+elif n == 12:
+    print("n is 12")
+else:
+    print("n is", n)
